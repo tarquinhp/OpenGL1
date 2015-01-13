42a715fa0eea9b8edd869489e97c02f03318e49d
tarquinhp <tarquinhp@gmail.com>
2015-01-13 16:01:50 +0000

    added window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <SDL.h>
-
+#include <cstdlib>
 
 int main()
 {
+    if (SDL_Init(SDL_INIT_VIDEO))
+    {
+        std::cerr<<"error init SDL/n";
+        exit(EXIT_FAILURE);
+    }
 
-    std::cout<<"Hello world\n";
+    SDL_Rect screenSize;
+    SDL_GetDisplayBounds(0,&screenSize);
+
+    SDL_Window *window = SDL_CreateWindow(
+                "OpenGL Demo",
+                SDL_WINDOWPOS_CENTERED,
+                SDL_WINDOWPOS_CENTERED,
+                screenSize.w/2,
+                screenSize.h/2,
+                SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+                );
+
+    SDL_Delay(10000);
+
+
+
 }
